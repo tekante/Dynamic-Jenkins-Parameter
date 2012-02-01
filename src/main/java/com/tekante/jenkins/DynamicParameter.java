56d836777a5c2894627b84d8c84488c722acb1f4
Chris Johnson <git@tekante.com>
2012-02-01 10:53:14 -0500

    Adjustment to allow doFillXyzItems method to work and to allow the values to fill to be pulled from the stored parameter configuration

--- src/main/java/com/tekante/jenkins/DynamicParameter.java
+++ src/main/java/com/tekante/jenkins/DynamicParameter.java
@@ -1,42 +1,38 @@
 package com.tekante.jenkins;
 
 import hudson.Extension;
-import org.kohsuke.stapler.QueryParameter;
 
-import hudson.model.Describable;
-import hudson.model.Descriptor;
 import hudson.model.Hudson;
 import hudson.model.ParameterValue;
 import hudson.model.ParameterDefinition;
+import hudson.model.ParametersDefinitionProperty;
 import hudson.model.StringParameterValue;
-import hudson.model.User;
-import hudson.model.ParameterDefinition.ParameterDescriptor;
 import hudson.util.ListBoxModel;
+import hudson.model.Job;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import net.sf.json.JSONObject;
 
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
-import org.kohsuke.stapler.export.Exported;
 
 public class DynamicParameter extends ParameterDefinition {
-	static final long serialVersionUID = 1;
+	static final long serialVersionUID = 2;
     public String value = "";
+    public String dynamicValue = "";
+    public String valueOptions;
+    public String dynamicValueOptions;
+    public String secondName;
     
 	@DataBoundConstructor
-	public DynamicParameter(String name, String description) {
+	public DynamicParameter(String name, String description, String valueOptions, String dynamicValueOptions, String secondName) {
 		super(name, description);
-	}
-
-	@Exported
-	public String getValue() {
-		return "two";
+		this.secondName = secondName;
+		this.valueOptions = valueOptions;
+		this.dynamicValueOptions = dynamicValueOptions;
 	}
 
 	@Extension
@@ -45,12 +41,48 @@ public class DynamicParameter extends ParameterDefinition {
 		public String getDisplayName() {
 			return "Dynamic Parameter";
 		}
-		
+
+		private DynamicParameter getDynamicParameter() {
+			String containsJobName = getCurrentDescriptorByNameUrl();
+			String jobName = java.net.URLDecoder.decode(containsJobName.substring(containsJobName.lastIndexOf("/") + 1));
+			Job<?,?> j = Hudson.getInstance().getItemByFullName(jobName, hudson.model.Job.class);
+			if (j != null) {
+				ParametersDefinitionProperty pdp = j.getProperty(hudson.model.ParametersDefinitionProperty.class);
+				List<ParameterDefinition> pds = pdp.getParameterDefinitions();
+				for (ParameterDefinition pd : pds) {
+					if (this.isInstance(pd)) {
+						return (DynamicParameter) pd;
+					}
+				}
+			}
+			return null;
+		}
+
 		public ListBoxModel doFillValueItems() {
+			Logger l = Logger.getLogger(DynamicParameter.class.getName());
 			ListBoxModel m = new ListBoxModel();
-			m.add("one");
-			m.add("two");
-			m.add("three");
+
+			DynamicParameter dp = this.getDynamicParameter();
+			if (dp != null) {
+				for (String s : dp.valueOptions.split("\\r?\\n")) {
+					m.add(s);
+				}
+			}
+			return m;
+		}
+
+		public ListBoxModel doFillDynamicValueItems(@QueryParameter String value) {
+			Logger l = Logger.getLogger(DynamicParameter.class.getName());
+			ListBoxModel m = new ListBoxModel();
+
+			DynamicParameter dp = this.getDynamicParameter();
+			if (dp != null) {
+				for (String s : dp.dynamicValueOptions.split("\\r?\\n")) {
+					if (s.indexOf(value) == 0) {
+						m.add(s);
+					}
+				}
+			}
 			return m;
 		}
 	}
